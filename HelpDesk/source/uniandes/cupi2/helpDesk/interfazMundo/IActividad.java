6dd6dd70b031f3e5f53b17873690f5b3a1feaef9
nicolasstucki <nicolasstucki@3c005732-1a59-11de-9308-ab3afd9c0aba>
2009-04-19 16:16:07 +0000

    Implementacion Grefos, parte 1

---
+++ HelpDesk/source/uniandes/cupi2/helpDesk/interfazMundo/IActividad.java
@@ -0,0 +1,8 @@
+package uniandes.cupi2.helpDesk.interfazMundo;
+
+public interface IActividad {
+
+	public int darNumeroVecesEjecutada();
+
+	public float darPromedioTiempo();
+}
---
+++ HelpDesk/source/uniandes/cupi2/helpDesk/interfazMundo/IGrafo.java
@@ -0,0 +1,15 @@
+package uniandes.cupi2.helpDesk.interfazMundo;
+
+public interface IGrafo {
+
+	public IActividad darActividad(String nombre);
+	
+	public IActividad[] darActividadesCriticas();
+	
+	public IActividad[] darActividadesMasLentas();
+	
+	public IIterador darListaActividadesPorTiempo();
+	
+	public float darTiempoPromedioEspera();
+	
+}
